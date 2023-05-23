07bf9f244c62b078469db33c959061ab158d46ef
kbondar17 <aa@bb.mail.ru>
2023-05-23 19:18:00 +0300

    add ip

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM python:3.6
+FROM python:3.9
 LABEL maintainer="lorenz.vanthillo@gmail.com"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 EXPOSE 8080
+ENV VERSION=1
 ENTRYPOINT ["python"]
 CMD ["app/app.py"]
--- app/app.py
+++ app/app.py
@@ -1,17 +1,28 @@
-from flask import Flask,render_template
+import os
+
+from flask import Flask, render_template, request
 import socket
 
+VERSION = os.environ["VERSION"]
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
+        return render_template(
+            "index.html",
+            hostname=host_name,
+            ip=host_ip,
+            version=VERSION,
+            user_ip=request.remote_addr,
+        )
     except:
-        return render_template('error.html')
+        return render_template("error.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
